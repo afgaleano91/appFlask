f306a411f803ab059af080e00f4c48aadf7e6e64
Andres Galeano <afgaleano91@misena.edu.co>
2020-09-09 01:54:11 -0500

    Se añade funcion para guardar y obtener cookies del browser

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
-#Se establece la ruta de index
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+#Se establece la ruta de index
+@app.route('/hello')
 def hello():
-    return 'Hello world Flask'
+    user_ip = request.cookies.get('user_ip')
+    return f'Hello world Flask, your IP is {user_ip}'
