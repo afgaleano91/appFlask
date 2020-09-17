2cb0a4e3c1f12870a0b9be87762d1a359e2e3e84
Andres Galeano <afgaleano91@misena.edu.co>
2020-09-17 00:56:23 -0500

    Se generan templates y css

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Comprar cafe', 'Entregar registro', 'Programar afiliaciones']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
 @app.route('/')
 def index():
+    # raise(Exception('500 error'))
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
@@ -16,4 +27,9 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return f'Hello world Flask, your IP is {user_ip}'
+    context = {
+        'user_ip' : user_ip,
+        'todos' : todos,
+    }
+
+    return render_template('hello.html', **context)
