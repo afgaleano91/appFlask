d302f1d2597cbff03cfb75bb1c82bfd7b0c7f34e
Andres Galeano <afgaleano91@misena.edu.co>
2020-10-03 01:07:22 -0500

    Se añade libreria para login y alerts

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -7,6 +10,12 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SECRET SUPER'
 
 todos = ['Comprar cafe', 'Entregar registro', 'Programar afiliaciones']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    summit = SubmitField('Enviar')
+    
 
 @app.errorhandler(404)
 def not_found(error):
@@ -28,12 +37,25 @@ def index():
 
 
 #Se establece la ruta de index
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip' : user_ip,
         'todos' : todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con exito!')
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
